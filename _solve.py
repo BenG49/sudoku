bedd0ef9a56b5ab81574c56c46527bcfa53f5d8c
BenG49 <64862590+BenG49@users.noreply.github.com>
2021-12-03 20:47:47 -0500

    Add comments

--- _solve.py
+++ _solve.py
@@ -6,53 +6,50 @@
 - treat n numbers sharing n tiles in a square as filled squares
 '''
 
+# global variable used to measure algorithm
 backtracks = 0
 
 # given position of a guess
-def solve(self, pos: tuple = None, val: int = None):
+def solve(self, guess_pos: tuple = None, guess_val: int = None):
 	global backtracks
 
 	# deepcopy in order to not have to revert moves
 	s = self.copy()
 
-	if pos:
-		s[pos] = val
+	if guess_pos:
+		s[guess_pos] = guess_val
 
 	changed = True
 	# keep track of mask lines that have been found
 	lines = []
+	# iterate until no changes are made
 	while changed:
 		changed = False
 
-		# look for rows or cols where theres only one valid place in row/col
+		# look for rows or cols with one valid place in row/col
 		for n in range(s.len):
-			# check rows and cols
-			r_pos, c_pos = -1, -1
-
 			for j in range(s.len):
+				row_pos, col_pos = -1, -1
+
 				for i in range(s.len):
-					# uneccessary optimization
-					if not r_pos and not c_pos:
+					# already found repeats in row and col
+					if not row_pos and not col_pos:
 						break
 
-					# cannot have multiple in the same row/col
-					# set to None if already set
+					# set to none if n has already been found in row/col
 					if s.get_mask(n, i, j):
-						r_pos = i if r_pos == -1 else None
+						row_pos = i if row_pos == -1 else None
 
 					if s.get_mask(n, j, i):
-						c_pos = i if c_pos == -1 else None
+						col_pos = i if col_pos == -1 else None
 
-				# there was only one possible pos in the row/col, so it must be n
-				if r_pos and r_pos != -1:
-					s[r_pos, j] = n
+				# only one possible placement in the row/col, must be n
+				if row_pos and row_pos != -1:
+					s[row_pos, j] = n
 					changed = True
-				if c_pos and c_pos != -1:
-					s[j, c_pos] = n
+				if col_pos and col_pos != -1:
+					s[j, col_pos] = n
 					changed = True
-
-				# reset pos
-				r_pos, c_pos = -1, -1
 
 		# loop through every square and look for only one valid spot
 		for y in range(0, s.len, s.boxlen):
@@ -69,22 +66,17 @@ def solve(self, pos: tuple = None, val: int = None):
 					if not s.iter_square(x, y, f) and found is not None:
 						s[found] = n
 
-		# look through every square and check if there is a line
-		# in the mask
-		# ex
-		# | = |
-		# |== |
-		# |••=|
+		# look through every box and check if there is a line
 		for y in range(0, s.len, s.boxlen):
 			for x in range(0, s.len, s.boxlen):
 				# loop through every number
 				for n in range(s.len):
 					t = s.mask_line(x, y, n)
 
-					if t is not None:
-						if t not in lines:
-							changed = True
-							lines.append(t)
+					# if a line was found and not previously found
+					if t is not None and t not in lines:
+						changed = True
+						lines.append(t)
 
 						s.update_line(x, y, n, t)
 
@@ -97,10 +89,9 @@ def solve(self, pos: tuple = None, val: int = None):
 	for n in range(s.len):
 		# loop through every row
 		for y in range(s.len):
-			in_row = False
-
-			# check that there is at least one valid position
+			# check that there is at least one valid position in the row
 			for x in range(s.len):
+				# possible placement
 				if s.isempty(x, y) and s.get_mask(n, x, y):
 					# recurse with guess
 					tmp = s.solve((x, y), n)
@@ -108,10 +99,13 @@ def solve(self, pos: tuple = None, val: int = None):
 					# if board was valid, return
 					if tmp:
 						return tmp
-				elif s[x, y] == n:
-					in_row = True
 
-			# no possible positions in row, invalid
-			if not in_row:
+				elif s[x, y] == n:
+					# if in row, there should be no more valid positions
+					break
+
+			# if did not break (n was not found in row)
+			else:
+				# no possible positions in row, invalid
 				backtracks += 1
 				return None
